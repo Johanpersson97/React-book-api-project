830ffb8e88380c0bb7482d7085e3cca648d7a3b1
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-05-19 13:41:51 +0200

    Bootstrap Test

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App container">
       <header className="App-header">
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
