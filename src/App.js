b8f79db32a2c923a80116c7da448e5401775701c
Gudster <g.fristedt@gmail.com>
2023-05-19 15:20:02 +0200

    lade till book_icon_large på framsidan

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import ToggleBookshelf from './toggleBookshelf';
 function App() {
   return (
     <header className="container mt-3">
-      <img src="%PUBLIC_URL%/book_icon_large.png" className="float-start"></img>
       <div className="float-end">
         <ToggleBookshelf />
       </div>
-      <h1 className="display-2 mb-0">Bookster</h1>
-      <p className="ms-1 h5 mt-0">Your online bookshelf</p>
+      <div className='d-flex' >
+        <img src={require('./book_icon_large.png')} id='book_icon' />
+        <div className='ms-2'>
+          <h1 className="display-2 mb-0">Bookster</h1>
+          <p className="ms-1 h5 mt-0">Your online bookshelf</p>
+          </div>
+      </div>
     </header>
   );
 }
