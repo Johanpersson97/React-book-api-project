2493b6a622a9483fc7aba39b963e05ec5065f1e5
Gudster <g.fristedt@gmail.com>
2023-05-22 14:21:09 +0200

    Försökte fixa formContainer

--- src/formContainer.js
+++ src/formContainer.js
@@ -1,16 +1,25 @@
-import React, { useState} from 'react';
+import React, { useState, useRef } from 'react';
 import Book from './bookListItem';
 
 const InputForm = () => {
   const apiKey = 'AIzaSyCnBU2K5epe8KRSljZmLjfF0KcMh_uueCA';
- 
-  const [searchTerm, setSearchTerm] = useState('');
+
+  const [searchTerm, setSearchTerm] = useRef('');
   const [books, setBooks] = useState([]);
 
-  
+  const handleSearchChange = (event) => {
+    const { value } = event.target;
+    setSearchTerm(value);
 
-  const handleSearch = () => {
-    // API anrop 
+    if (value.length > 2) {
+      searchTerm.current.value
+      handleSearch({searchTerm})
+    }
+  };
+
+  const handleSearch = (searchTerm) => {
+    // API anrop
+
 
     fetch(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${apiKey}`)
       .then(response => response.json())
@@ -37,7 +46,7 @@ const InputForm = () => {
   return (
     <div>
       <div class="mt-5 mx-auto w-50">
-        <input id="search-field" class="form-control form-control-lg"  type="text" value={searchTerm} onChange={e => setSearchTerm(e.target.value)} />
+        <input id="search-field" class="form-control form-control-lg"  type="text" ref={searchTerm} onKeyUp={handleSearchChange} />
 
         <ul id="movie-list" class="list-group mt-3">
           {books.map(book => (<Book key={book.id} item= {book}/>))}
