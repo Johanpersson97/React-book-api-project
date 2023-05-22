cf5fda158fe1b627fe2ac32078facae26bf6cf2d
Gudster <g.fristedt@gmail.com>
2023-05-22 16:36:42 +0200

    fixade till offcanvas bookshelf

--- src/toggleBookshelf.js
+++ src/toggleBookshelf.js
@@ -4,11 +4,11 @@ import Bookshelf from "./bookshelf"
 const ToggleBookshelf = () => {
   return (
     <div>
-      <button id="toggleBookshelfButton" className="position-absolute top-0 end-0" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">My Bookshelf</button>
-      <div className="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+      <button id="toggleBookshelfButton" className="position-absolute top-0 end-0" tabIndex={1046} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">My Bookshelf</button>
+      <div className="offcanvas offcanvas-end text-bg-dark" tabIndex={-1} id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
         <div className="offcanvas-header">
         <h5 className="offcanvas-title" id="offcanvasRightLabel">My bookshelf</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <button type="button" className="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
         <div className="offcanvas-body">
           <Bookshelf />
