714c9ad20e353140dfbb97010dc4c8512010305d
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-05-25 11:02:21 +0200

    skapat komponenter för har läst och betyg

---
+++ src/hasReadButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const HasReadButton = () => {
+
+
+
+
+  return (
+  <div className="form-check">
+    <input className="form-check-input" type="checkbox" value="" id="flexCheckDefault"/>
+    <label className="form-check-label" for="flexCheckDefault">
+    Har Läst
+    </label>
+  </div>
+  );
+}
+
+export default HasReadButton;
