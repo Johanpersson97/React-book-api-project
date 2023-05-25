82b63efd9f682b9194f71ff100ccaa3c3a752856
Melindaljungdell <102459129+Melindaljungdell@users.noreply.github.com>
2023-05-25 11:57:33 +0200

    fixat modal

--- src/bookListItem.js
+++ src/bookListItem.js
@@ -1,13 +1,23 @@
-import React from 'react';
-import StarRating from './rating';
+import React , { useState } from 'react';
+import BookInfo from './bookInfo'; 
+
 
 export default function Book(props) {
+  
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
+
   return (
     <div>
-      <li className="list-group-item list-group-item-action list-group-item-dark d-flex p-0 align-items-center">
-        <img className="me-2 small-image"
-          src={props.item.smallImg !== undefined ? props.item.smallImg : require("./undefined.png")}
-          alt="Book cover" />
+      <li className="list-group-item list-group-item-action list-group-item-dark d-flex p-0 align-items-center" onClick={handleOpenModal}>
+        <img className="me-2" src={props.item.img} />
         <div className="d-flex flex-column">
           <p className="m-0">
             {props.item.title}
@@ -19,8 +29,9 @@ export default function Book(props) {
         <span className="ms-auto me-2">
           {props.item.publishedDate}
         </span>
-        <StarRating />
       </li>
+      {showModal && <BookInfo key={props.item.id} item={props.item} closeModal={handleCloseModal} />}
     </div>
-  )
+  );
+
 }
