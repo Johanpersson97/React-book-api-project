3cb375520b2466a7f97c1c12455b57daf655a375
Johanpersson97 <89947427+Johanpersson97@users.noreply.github.com>
2023-05-30 10:46:47 +0200

    Tar endast bort en bok istället för alla -buggfix

--- src/bookshelf.js
+++ src/bookshelf.js
@@ -18,7 +18,7 @@ const Bookshelf = () => {
   };
 
   const handleRemoveBook = (book) => {
-    const updatedBooks = books.filter((b) => b.id !== book.id);
+    const updatedBooks = books.filter((b) => b.title !== book.title);
     localStorage.setItem('books', JSON.stringify(updatedBooks));
     setSelectedBook(null);
   };
